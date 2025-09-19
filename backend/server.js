91c6cb772d5d883b5aebad0e6855eeb6e14a6cdf
Aksh <aksh87@gmail.com>
2025-09-18 23:01:34 -0700

    Initial project structure with basic frontend and backend

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const mongoose= require('mongoose')
+const cors = require('cors')
+
+require('dotenv').config()
+const port = process.env.PORT||3000;
+
+app.use(cors())
+app.use(express.json())
+
+//connect to MongoDB
+//Note: We'll use a local database for now and a Render-managed one later 
+
+const uri = process.env.MONGODB_URI ||'mongodb://localhost:27017/ecommerce'
+mongoose.connect(uri)
+.then(()=>console.log('MongoDB connected..'))
+.catch(err=>console.error(err))
+
+app.get('/',(req,res)=>{
+    res.send('Backend is running !');
+})
+
+app.listen(port,()=>{
+    console.log(`Server is running on post :${port}`)
+})
