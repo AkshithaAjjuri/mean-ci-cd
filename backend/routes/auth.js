6e8a33dafab0348fb4d5af354627132fd337269c
Aksh <aksh87@gmail.com>
2025-09-19 02:11:54 -0700

    feat: Add sign up module

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,12 @@
 const router = require('express').Router()
 const User= require('../models/user.models');
 
-router.route('/signup').post(async(req,res)=>{
+router.route('/signup',async(req,res)=>{
     const {username , password}= req.body;
     const newUser = new User ({ username,password})
     try{
         await newUser.save();
-        res.status(400).json('Error:'+err)
+        res.status(400).send('user registered successfully')
     }
     catch(err){
         res.status(400).send('Error registrating user'+err.message)
