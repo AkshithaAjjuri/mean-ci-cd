a7e087c226459b51162d4c59a11c72631cdaf202
Aksh <aksh87@gmail.com>
2025-09-19 01:53:23 -0700

    feat: Add sign up module

--- backend/models/user.models.js
+++ backend/models/user.models.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const UserSchema= new mongoose.Schema({
     username:{tyepe:String, require:true , unique: true},
-    password:{ type:String, require:true }
+    password:{ type:String, required:true }
 })
 
 module.exports = mongoose.model('User', UserSchema)
